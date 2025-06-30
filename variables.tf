f54f0bcf806f4ad5629396fee0a9365a1baf34a5
zerotohero200222 <aebharath17@gmail.com>
2025-06-30 21:03:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,32 +1,6 @@
 variable "aws_region" {
-  type    = string
-  default = "us-east-1"
+  type        = string
+  default     = "us-east-1"
+  description = "AWS region to deploy resources."
 }
 
-resource "random_pet" "lb_name" {
-  length = 2
-}
-
-resource "random_string" "log_prefix" {
-  length  = 8
-  special = false
-  upper   = false
-}
-
-resource "random_string" "env_tag" {
-  length  = 5
-  special = false
-  upper   = false
-}
-
-output "generated_lb_name" {
-  value = "${random_pet.lb_name.id}-alb"
-}
-
-output "log_prefix" {
-  value = random_string.log_prefix.result
-}
-
-output "environment_tag" {
-  value = random_string.env_tag.result
-}
