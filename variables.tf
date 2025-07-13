a924ff6482ea379016b6c91f1334d1fb5da52f2b
zerotohero200222 <aebharath17@gmail.com>
2025-07-13 23:15:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,9 @@
 variable "aws_region" {
   type        = string
-  default     = "us-east-1"
   description = "AWS region to deploy resources."
 }
 
+variable "env" {
+  type        = string
+  description = "Environment name"
+}
